15f3ac0a7e09ad8044f9f9940b2a4e5962743d76
Michael L. Collard <collard@uakron.edu>
2021-02-10 10:54:07 -0500

    Extract key parsing

--- DockerReport.cpp
+++ DockerReport.cpp
@@ -38,21 +38,12 @@ int main() {
         if (pc == buffer.cend()) {
             break;
         } else if (isKey(pc, invalue)) {
-            // parse key
-            auto endpc = buffer.cend();
-            auto endname = std::find_if_not(pc, endpc, [] (char c) { return isalnum(c) || c == '_' || c == '.'; });
-            if (endname == endpc)
-                return 1;
-            std::string name(pc, endname);
-            pc = endname;
-            pc = std::find_if_not(pc, endpc, [] (char c) { return isspace(c); });
-            if (*pc != ':')
-                return 1;
-            std::advance(pc, 1);
-            pc = std::find_if_not(pc, endpc, [] (char c) { return isspace(c); });
-            invalue = true;
 
-            // update counters and version
+            // parse key
+            std::string name;
+            pc = parseKey(pc, buffer.cend(), name, invalue);
+
+            // update docker counters and version
             if (name == "version") {
                 inversion = true;
             } else {
@@ -68,6 +59,7 @@ int main() {
                     ++opensuse_count;
                 }
             }
+
         } else if (isValue(pc, invalue)) {
             // parse value
             auto endpc = buffer.cend();
--- yaml_parser.hpp
+++ yaml_parser.hpp
@@ -15,4 +15,7 @@ bool isKey(std::string::const_iterator pc, bool invalue);
 // YAML parsing is at a value
 bool isValue(std::string::const_iterator pc, bool invalue);
 
+// Parse a YAML key
+std::string::const_iterator parseKey(std::string::const_iterator pc, std::string::const_iterator end, std::string& name, bool& invalue);
+
 #endif
