23d22ed5a130a6a43bb49d6c441407356511436e
Michael L. Collard <collard@uakron.edu>
2021-02-14 17:03:41 -0500

    Add method isKey() and call in Docker Report

--- YAMLParser.cpp
+++ YAMLParser.cpp
@@ -5,3 +5,10 @@
 */
 
 #include "YAMLParser.hpp"
+#include "yaml_parser.hpp"
+
+// YAML parsing is at a key
+bool YAMLParser::isKey(std::string::const_iterator pc, bool invalue) {
+
+    return ::isKey(pc, invalue);
+}
